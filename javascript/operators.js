fb70209f4d59b77fab4dd70ab2a00511124ed62a
secret <lordhunter017@gmail.com>
2025-08-29 20:34:38 +0545

    js suru'

---
+++ javascript/operators.js
@@ -0,0 +1,51 @@
+// // ! Operators in JavaScript
+// // * Arithmetic Operators + - * / % ++ -- 
+// let x = 10
+// let y = 3
+
+// let sum = x + y
+// let diff = x -y
+// let mul = x * y
+// let div = x / y
+// let mod = x % y
+// let inc = ++x
+// let dec = --y 
+// // * Assignment Operators = += -= *= /= %=
+// let number = 1
+// number += 5
+
+// // * Comparison Operators == === != !== > < >= <=
+
+// // * Logical Operators && || !
+// // and, or not 
+// // &&, ||, !
+// // * String Operators + +=
+// // * Conditional (Ternary) Operator ? :
+
+// let a = 10
+// let b = "10"
+// // number, String, boolean, null, undefined, symbol
+// let c = a === b
+// console.log(c) // false
+
+// WAP to add, subtract, multiply, divide two numbers and find the remainder.
+
+const a = 20;
+const b = 30;
+const sum = a +b;
+const diff = a - b;
+const mul = a * b;
+const div = a / b;
+const mod = a % b;
+
+console.log("Sum:" + sum + "\n" + "Difference:" + diff + "\n" + "Multiplication:" + mul + "\n" + "Division:" + div + "\n" + "Modulus:" + mod);
+
+
+// WAP to find the average of two numbers
+const avg = (a + b) /2;
+console.log("Average:" + avg);
+
+// WAP to find the area of a rectangle, square, circle
+// WAP to find the area and circumference of a circle
+
+
